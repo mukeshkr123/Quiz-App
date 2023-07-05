d0900c801349c711632cf98f05344eda74055549
mukesh <mukeshmehta2041@gmail.com>
2023-07-05 16:54:44 +0530

    adding the quiz page

--- src/components/Quix.jsx
+++ src/components/Quix.jsx
@@ -1,5 +1,19 @@
+import { useState } from "react";
+import Questions from "../helpers/Questions";
+
 const Quix = () => {
-  return <div>Quix</div>;
+  const [currentQuestion, setCurrentQuestion] = useState(0);
+
+  return (
+    <div className="quiz">
+      <h1>Question {currentQuestion + 1}</h1>
+      <button>{Questions[currentQuestion].prompt}</button>
+      <button>{Questions[currentQuestion].optionA}</button>
+      <button>{Questions[currentQuestion].optionB}</button>
+      <button>{Questions[currentQuestion].optionC}</button>
+      <button>Next</button>
+    </div>
+  );
 };
 
 export default Quix;
